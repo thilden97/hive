31a2c8ac971f75760f472d3b2656a520c7d6e87c
thilden97 <93315459+thilden97@users.noreply.github.com>
2021-12-07 13:30:27 +0200

    Create ft_memdel.c

---
+++ ft_memdel.c
@@ -0,0 +1,11 @@
+
+#include "Libft.h"
+
+void    ft_memdel(void **ap)
+{       
+        if (ap != NULL)
+        {       
+                free(*ap);
+                *ap = NULL;
+        }       
+}
