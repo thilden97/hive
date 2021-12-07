8eba93c98556201973db1526f39fb77db50f9ae4
thilden97 <93315459+thilden97@users.noreply.github.com>
2021-12-07 13:52:50 +0200

    Create ft_striteri.c

---
+++ ft_striteri.c
@@ -0,0 +1,15 @@
+
+#include "libft.h"
+
+void    ft_striter(char *s, void (*f)(char *))
+{       
+        if (s != NULL && f != NULL)
+        {       
+                while (*s)
+                {       
+                        f(s);
+                        s++;
+                }
+        }       
+}       
+~      
