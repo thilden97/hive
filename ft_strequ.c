dfa49657920f8509c0ee09bd7c7dc3182f111d9b
thilden97 <93315459+thilden97@users.noreply.github.com>
2021-12-07 14:10:55 +0200

    Create ft_strequ.c

---
+++ ft_strequ.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+int     ft_strequ(char const *s1, char const *s2)
+{       
+        if (!s1 || !s2)
+                return (0);
+        if (ft_strlen(s1) != ft_strlen(s2))
+                return (0);
+        while (*s1 && *s2)
+        {       
+                if (*s1 != *s2)
+                        return (0);
+                s1++;
+                s2++;
+        }       
+        return (1);
+}     
